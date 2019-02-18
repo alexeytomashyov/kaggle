1b740393f5151424838fe7663407f3f32f523253
alexeytomashyov <alexeytomashyov@users.noreply.github.com>
2019-02-18 23:46:55 +0300

    commit

--- xgboost.py
+++ xgboost.py
@@ -1,13 +1,14 @@
 # %%
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer as Imputer
 
 data = pd.read_csv('train.csv')
 data.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = data.SalePrice
 X = data.drop(['SalePrice'], axis=1).select_dtypes(exclude=['object'])
-train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix(), test_size=0.25)
+train_X, test_X, train_y, test_y = train_test_split(X.values, y.values, 
+                                                    test_size=0.25)
 
 my_imputer = Imputer()
 train_X = my_imputer.fit_transform(train_X)
@@ -29,12 +30,7 @@ print("Mean Absolute Error : " + str(mean_absolute_error(predictions, test_y)))
 
 # %%
 # Model tuning
-my_model = XGBRegressor(n_estimators=1000)
-my_model.fit(train_X, train_y, early_stopping_rounds=5, 
-             eval_set=[(test_X, test_y)], verbose=False)
-
-# %%
-# changing learning rate
+# Choosing number of estimators and learning rate
 my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
 my_model.fit(train_X, train_y, early_stopping_rounds=5, 
              eval_set=[(test_X, test_y)], verbose=False)
